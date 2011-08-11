4c8b6406c0601afc59c1a888b1c1d4377351ceed
Carlos Brando <eduardobrando@gmail.com>
2011-08-11 14:06:40 -0300

    Rollback supervisor.

--- src/riak_kv_pb_socket_sup.erl
+++ src/riak_kv_pb_socket_sup.erl
@@ -27,14 +27,18 @@
 -export([start_link/0, init/1, stop/1]).
 -export([start_socket/0, start_socket/1]).
 
-start_socket() -> 
+start_socket() ->
     start_socket([]).
 
-start_socket(SslOpts) -> 
-    supervisor:start_child(?MODULE, [SslOpts]).
+start_socket(_SslOpts) ->
+    % supervisor:start_child(?MODULE, [SslOpts]).
+    supervisor:start_child(?MODULE, []).
 
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+% start_link(SslOpts) ->
+%     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 stop(_S) -> ok.
 
